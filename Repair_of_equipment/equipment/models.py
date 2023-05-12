b64e1aef605ecca08406bef82541957b3e58b8e1
Vadim Roziznan <roziznan_v@mail.ru>
2023-05-12 20:08:47 +0300

    12.05.23

--- Repair_of_equipment/equipment/models.py
+++ Repair_of_equipment/equipment/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Machine(models.Model):
     name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='photos')
+    category = models.CharField(max_length=50)
 
 
 class Archive(models.Model):
